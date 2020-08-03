5a05a047ec0bc349db9f3c36a61508044de9269f
James Cameron <quozl@laptop.org>
2020-08-03 16:26:34 +1000

    Apply copyright year

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sugar'
-copyright = u'2015-2017, SugarLabs'
+copyright = u'2015-2020, SugarLabs'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
